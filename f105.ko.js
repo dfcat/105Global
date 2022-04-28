a6cd4b8f2fabdc9b9519d2201f2bb1d36f41fec6
dr.ohrot <bakal_god@naver.com>
2022-04-29 02:13:55 +0900

    add: meta files for each lang; add: f105 langs

---
+++ f105.ko.js
@@ -0,0 +1,72 @@
+{
+  "lang": "ko",
+  "common": {
+    "option": "옵션",
+    "damage": "피해 증가",
+    "buff": "버프력",
+    "availableLevel": "레벨 제한",
+    "status": "스테이터스",
+    "buffOption": "버퍼 전용 옵션"
+  },
+  "main": {
+    "guide": {
+      "warning": "던파캣 F105은 간단하지만 조작법을 알면 더 편해집니다",
+      "ask": "가이드를 보시겠습니까?",
+      "nomore": "다시 보지 않기",
+      "agree": "예",
+      "disagree": "아니요"
+    }
+  },
+  "preset": {
+    "title": "프리셋",
+    "loaded": "불러왔습니다",
+    "delete": "삭제",
+    "deleted": " 프리셋이 삭제되었습니다",
+    "save": "저장",
+    "saved": "저장되었습니다",
+    "create": "새 프리셋 만들기",
+    "deleteConfirm": "삭제하면 복구할 수 없습니다. 정말로 삭제하겠습니까?",
+    "empty": "아직 저장된 프리셋이 없습니다.",
+    "emptyGuide": "캐릭별로, 세팅별로 프리셋에 저장할 수 있습니다.",
+    "adblock": "애드블록을 이용하시면 최대 5개의 프리셋만 이용하실 수 있습니다"
+  },
+  "set": {
+    "placeholder": "이름 부분 검색 (ex: 이 드 -> '드'래곤 슬레'이'어)",
+    "releaseAllItems": "모두 해제",
+    "hideAll": "전부 숨기기",
+    "showAll": "전부 보이기",
+    "showWannabeOnly": "보고 싶은 세트만 보기",
+    "showAllSets": "세트 모두 보기",
+    "labelObjection": "컨셉별 추천 세팅은 정답이 아닙니다. 반박시 님 말이 맞습니다.",
+    "labelExclude": "특정 조건일 때 (아바타, 크리쳐, 근접캐릭 등) 좋은 장비는 제외되어 있습니다.",
+    "labelCustomizing": "자신의 상황에 맞춰서 세팅해주세요.",
+    "reference": "세팅 참고",
+    "setNameBefore": "세트 이름은",
+    "setNameAfter": "님이 창작하신 것으로 오피셜은 아닙니다."
+  },
+  "item": {
+    "growInfo": "성장 옵션",
+    "totalDamage": "성장 옵션 총 피해 증가",
+    "totalBuff": "성장 옵션 총 버프력"
+  },
+  "dungeon": {
+    "labelNamedSelectedItems": "현재 선택한 아이템을 드랍하는 네임드",
+    "labelNamedDesc": "네임드의 이름을 누르면 그 네임드가 주는 아이템을 보여줍니다",
+    "labelItems": "이 던전에서 얻을 수 있는 아이템",
+    "labelMonsterCount": "제가 만렙 찍고 던전을 돌게 되면 채워 넣겠습니다",
+    "drops": "드랍하는 아이템",
+    "modeRoad": "길 모드",
+    "modeBoss": "보스 모드",
+    "modeNamed": "모든 네임드 모드",
+    "modeMonster": "잡몹 모드",
+    "modeSelected": "네임드 모드",
+    "labelExtension1": "노블레스 코드와",
+    "labelExtension2": "파괴된 죽은자의 성은",
+    "labelExtension3": "위치가 매번 바뀝니다"
+  },
+  "dungeons": {
+    "normal": "일반 던전만 보기",
+    "extension": "상급 던전도 보기",
+    "all": "모든 던전 보기"
+  }
+}
